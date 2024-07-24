f52688334f0c33c5a1449cbc262db7c10a1ae69b
kerolles-khalaf <keroloskhalaf2@gmail.com>
2024-07-24 12:43:26 +0300

    Add files via upload

---
+++ MadLibs.py
@@ -0,0 +1,10 @@
+name = input("please input your name: ")
+age = input("please input your age: ")
+color = input("please input your favourite color: ")
+jop = input("please input your jop: ")
+
+
+print("my name is " + name)
+print("my age is " + age)
+print("my favourite color is " + color)
+print("my age is " + jop)
